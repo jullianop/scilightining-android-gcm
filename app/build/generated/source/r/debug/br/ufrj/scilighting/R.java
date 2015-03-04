c58ea6c1cf1e3ab9f31a47fa59d7481522895fc6
jullianop <jullianoo@hotmail.com>
2015-03-03 21:56:35 -0300

    Ajustando para GCM

--- app/build/generated/source/r/debug/br/ufrj/scilighting/R.java
+++ app/build/generated/source/r/debug/br/ufrj/scilighting/R.java
@@ -180,18 +180,19 @@ containing a value of this type.
     public static final class id {
         public static final int buttonRegister=0x7f070008;
         public static final int buttonRegisterNow=0x7f070005;
+        public static final int display=0x7f070014;
         public static final int editLoginRegister=0x7f070006;
         public static final int editPasswordRegister=0x7f070007;
         public static final int hybrid=0x7f070000;
-        public static final int imageView1=0x7f070015;
+        public static final int imageView1=0x7f070016;
         public static final int imagemview=0x7f07000b;
         public static final int itemtext1=0x7f07000d;
         public static final int itemtext2=0x7f07000e;
         public static final int itemtext3=0x7f07000c;
         public static final int itemtext4=0x7f07000f;
-        public static final int linearLayout1=0x7f070014;
+        public static final int linearLayout1=0x7f070015;
         public static final int listView2=0x7f070010;
-        public static final int menu_save=0x7f070019;
+        public static final int menu_save=0x7f07001a;
         public static final int message=0x7f070009;
         public static final int none=0x7f070001;
         public static final int normal=0x7f070002;
@@ -201,9 +202,9 @@ containing a value of this type.
         public static final int relativeLayout1=0x7f07000a;
         public static final int satellite=0x7f070003;
         public static final int terrain=0x7f070004;
-        public static final int textView1=0x7f070016;
-        public static final int textView2=0x7f070017;
-        public static final int textView3=0x7f070018;
+        public static final int textView1=0x7f070017;
+        public static final int textView2=0x7f070018;
+        public static final int textView3=0x7f070019;
     }
     public static final class layout {
         public static final int main=0x7f030000;
